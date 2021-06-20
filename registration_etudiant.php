616160ccad1614507cc226dc65dc699213c9264e
OULD AMARA Amine <ouldamaraamine@gmail.com>
2021-06-20 21:47:38 +0100

    starting working on course editor

--- registration_etudiant.php
+++ registration_etudiant.php
@@ -1,3 +1,101 @@
+<?php
+require_once "db.php";
+session_start();
+
+if (isset($_REQUEST['register_etd_btn'])) //button name "btn_register"
+{
+  $nom  = strip_tags($_REQUEST['txt_nom']);
+  $prenom  = strip_tags($_REQUEST['txt_prenom']);
+  $email    = strip_tags($_REQUEST['txt_email']);
+  $password  = strip_tags($_REQUEST['txt_password']);
+
+
+  if (empty($nom)) {
+    $errorMsg[] = "Entrez votre nom s'il vous plait";
+  } else if (empty($prenom)) {
+    $errorMsg[] = "Entrez votre prenom s'il vous plait";
+  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errorMsg[] = "Entrez votre email s'il vous plait";
+  } else if (empty($password)) {
+    $errorMsg[] = "Entrez votre mot de passe s'il vous plait";
+  } else if (strlen($password) < 6) {
+    $errorMsg[] = "Le mot de passe doit se composer d'au moins 7 caractéres";
+  } else {
+
+    $select_stmt = $db->prepare("SELECT email_etd FROM etudiant 
+										WHERE email_etd=:uemail"); // sql select query
+    $select_stmt->execute(array(':uemail' => $email)); //execute query 
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row['email_etd'] == $email) {
+      $errorMsg[] = "Desolé l'email exist déja ! ";  //check condition email already exists 
+    } else if (!isset($errorMsg)) //check no "$errorMsg" show then continue
+    {
+      $new_password = password_hash($password, PASSWORD_DEFAULT); //encrypt password using password_hash()
+
+      $insert_stmt = $db->prepare("INSERT INTO etudiant (nom_etd,prenom_etd,email_etd,mdp_etd) VALUES
+																(:unom,:uprenom,:uemail,:upassword)");     //sql insert query					
+
+      if ($insert_stmt->execute(array(
+        ':unom' => $nom,
+        ':uprenom'  => $prenom,
+        ':uemail'  => $email,
+        ':upassword' => $new_password
+      ))) {
+        $registerMsg = "Register Successfully..... "; //execute query success message
+        $_SESSION["email_etd"] = $row["id_etd"];  //session name is "user_login"
+        header("refresh:2; index.php");      //refresh 2 second after redirect to "welcome.php" page
+      }
+    }
+  }
+} else if (isset($_REQUEST['btn_ens_register'])) {
+  $nom  = strip_tags($_REQUEST['txt_nom']);
+  $prenom  = strip_tags($_REQUEST['txt_prenom']);
+  $email    = strip_tags($_REQUEST['txt_email']);
+  $password  = strip_tags($_REQUEST['txt_password']);
+
+
+  if (empty($nom)) {
+    $errorMsg[] = "Entrez votre nom s'il vous plait";
+  } else if (empty($prenom)) {
+    $errorMsg[] = "Entrez votre prenom s'il vous plait";
+  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errorMsg[] = "Entrez votre email s'il vous plait";
+  } else if (empty($password)) {
+    $errorMsg[] = "Entrez votre mot de passe s'il vous plait";
+  } else if (strlen($password) < 6) {
+    $errorMsg[] = "Le mot de passe doit se composer d'au moins 7 caractéres";
+  } else {
+
+    $select_stmt = $db->prepare("SELECT email_ens FROM enseignant WHERE email_ens=:uemail"); //sql select query
+    $select_stmt->execute(array(':uemail' => $email)); //execute query 
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row['email_ens'] == $email) {
+      $errorMsg[] = "Desolé l'email exist déja ! ";  //check condition email already exists 
+    } else if (!isset($errorMsg)) //check no "$errorMsg" show then continue
+    {
+      $new_password = password_hash($password, PASSWORD_DEFAULT); //encrypt password using password_hash()
+
+      $insert_stmt = $db->prepare("INSERT INTO enseignant	(nom_ens,prenom_ens,email_ens,mdp_ens) VALUES
+																(:unom,:uprenom,:uemail,:upassword)");     //sql insert query					
+
+      if ($insert_stmt->execute(array(
+        ':unom' => $nom,
+        ':uprenom'  => $prenom,
+        ':uemail'  => $email,
+        ':upassword' => $new_password
+      ))) {
+        $registerMsg = "Register Successfully..... "; //execute query success message
+        $_SESSION["email_ens"] = $row["id_ens"];  //session name is "user_login"
+        header("refresh:2; index.php");      //refresh 2 second after redirect to "welcome.php" page
+      }
+    }
+  }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,97 +103,176 @@
   <meta charset="UTF-8">
   <script src="RESSOURCES/js/registration.js"></script>
   <link rel="stylesheet" href="RESSOURCES/css/registration.css">
-  <title>Document</title>
+  <title>Registration</title>
 </head>
 
 <body>
-<?php
-    require('db.php');
-    session_start();
-
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['nom_etd'])) {
-        // removes backslashes
-
-        $nom_etd = stripslashes($_REQUEST['nom_etd']);
-        //escapes special characters in a string
-        $nom_etd = mysqli_real_escape_string($con, $nom_etd);
-
-        $prenom_etd = stripslashes($_REQUEST['prenom_etd']);
-        $prenom_etd = mysqli_real_escape_string($con, $prenom_etd);
-
-        $email_etd    = stripslashes($_REQUEST['email_etd']);
-        $email_etd    = mysqli_real_escape_string($con, $email_etd);
-
-        $mdp_etd = stripslashes($_REQUEST['mdp_etd']);
-        $mdp_etd = mysqli_real_escape_string($con, $mdp_etd);
-
-
-        $query    = "INSERT into `etudiant` (nom_etd, prenom_etd, email_etd, mdp_etd)
-                     VALUES ('$nom_etd', '$prenom_etd', '$email_etd' , '" . md5($mdp_etd) . "')";
-        $result   = mysqli_query($con, $query);
-        
-        if ($result) {
-          $_SESSION['email_etd'] = $email_etd;
-          // Redirect to user dashboard page
-          header("Location: index.php");
-        } else {
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
-                  </div>";
+  <nav style="background-color: white;">
+    <div class="logo">
+      <a href="index.php"><img src="RESSOURCES/css/img/logo.png" alt="logo tepee" style="height: 60px; width:auto;"></a>
+    </div>
+    <p>Avez-vous deja un compte? <a href="login.php">Connectez vous !</a> </p>
+  </nav>
+  <div class="container">
+    <div class="split left">
+      <h1>Étudiant</h1>
+      <?php
+      if (isset($errorMsg)) {
+        foreach ($errorMsg as $error) {
+      ?>
+          <div class="alert alert-danger">
+            <strong>WRONG ! <?php echo $error; ?></strong>
+          </div>
+        <?php
         }
-    } else {
-    ?>
-  <div class="tout">
-    <div class="cote-form">
-      <div class="logo">
-        <a href="index.php"><img src="RESSOURCES/css/img/logo.png" alt="logo tepee"></a>
-      </div>
-      <form class="form" name="myForm" action="" method="post">
+      }
+      if (isset($registerMsg)) {
+        ?>
+        <div class="alert alert-success">
+          <strong><?php echo $registerMsg; ?></strong>
+        </div>
+      <?php
+      }
+      ?>
 
-        <!-- -----------------------les deux boutons ---------------------------- -->
-
-        <div class="cc-selector">
-          <h3>choisissez votre type de Compte</h3>
-          <div class="choose">
-          <div>
-            <input id="etudiant" type="radio" name="type_inscription" value="etudiant" checked/>
-            <label class="drinkcard-cc etudiant" for="etudiant">Étudiant</label>
-          </div>
-          <div class="ou">ou</div>
-          <div>
-            <input id="enseignant" type="radio" name="type_inscription" value="enseignant" Onclick="location.href = 'registration_enseignant.php';" />
-            <label class="drinkcard-cc enseignant" for="enseignant">Enseignant</label>
+      <form class="form-etudiant" name="etd-form" action="" method="post">
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_nom" type="text" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Nom</label>
           </div>
         </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_prenom" type="text" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Prenom</label>
+          </div>
+        </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_email" type="email" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Email</label>
+          </div>
+        </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_password" type="password" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Mot de passe</label>
+          </div>
         </div>
 
-        <!-- -------------------------------------------------------- -->
+        <div class="condition">
+          <input type="checkbox" id="accepter">
+          <label for="accepter"> j'ai lu j'ai accepter les <a href="">Conditions generales d'utilisation</a>et <a href="">Politique de protection des donnees personnelles</a></label>
+        </div>
+
+        <input class="btn" type="submit" name="register_etd_btn" value="S'inscrire">
+      </form>
+    </div>
 
 
-        <input  type="text" class="login-input" name="nom_etd" placeholder="Nom" required> <br>
-        <input type="text" class="login-input" name="prenom_etd" placeholder="Prenom" required> <br>
-        <input type="email" class="login-input" name="email_etd" placeholder="Email" required> <br>
-        <input type="password" class="login-input" name="mdp_etd" placeholder="Mot de passe" required> <br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="split right">
+      <h1>Enseignant</h1>
+      <?php
+      if (isset($errorMsg)) {
+        foreach ($errorMsg as $error) {
+      ?>
+          <div class="alert alert-danger">
+            <strong>WRONG ! <?php echo $error; ?></strong>
+          </div>
+        <?php
+        }
+      }
+      if (isset($registerMsg)) {
+        ?>
+        <div class="alert alert-success">
+          <strong><?php echo $registerMsg; ?></strong>
+        </div>
+      <?php
+      }
+      ?>
+
+
+
+
+      <form class="form-enseignant" name="ens-form" action="" method="post">
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_nom" type="text" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Nom</label>
+          </div>
+        </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_prenom" type="text" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Prenom</label>
+          </div>
+        </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_email" type="email" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Email</label>
+          </div>
+        </div>
+        <div>
+          <div class="field-container">
+            <input class="field-input" id="inputid" name="txt_password" type="password" placeholder=" ">
+            <label class="field-placeholder" for="inputName">Mot de passe</label>
+          </div>
+        </div>
         <div class="condition">
           <input type="checkbox" id="accepter" required>
-          <label for="accepter"> j'ai lu j'ai accepter les <a href="">Conditions generales d'utilisation</a> et <a
-              href="">Politique de protection des donnees personnelles</a> la </label>
+          <label for="accepter"> j'ai lu j'ai accepter les <a href="">Conditions generales d'utilisation</a> et <a href="">Politique de protection des donnees personnelles</a></label>
         </div>
-        <br>
-        <input type="submit" name="submit" value="S'inscrire">
-        <p>Avez-vous deja un compte? <a href="login.php">Connectez vous !</a> </p>
+        <input class="btn" type="submit" name="btn_ens_register" value="S'inscrire">
       </form>
 
-    </div>
-    <div class="image">
-      <img src="RESSOURCES/css/img/register_etd.png" alt="étudiant">
+
+
+
     </div>
   </div>
-  <?php
-    }
-    ?>
+
+
+
 </body>
 
 </html>
+
+
+
+
+
+
+
+
+
+<script>
+  const left = document.querySelector('.left')
+  const right = document.querySelector('.right')
+  const container = document.querySelector('.container')
+
+  left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+  left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+  right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+  right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+</script>
